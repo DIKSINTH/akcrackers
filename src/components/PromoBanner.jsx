f1ae060de8d10fca9e9035e5a4cbecc1b75f4b4b
DIKSINTH <diksinth1@gmail.com>
2026-02-12 13:52:32 +0530

    start-project

--- src/components/PromoBanner.jsx
+++ src/components/PromoBanner.jsx
@@ -4,9 +4,9 @@ import img2 from "../assets/images/4.jpg";
 export default function PromoBanner() {
   return (
     <section className="w-full bg-[#fefeff] py-8">
-      <div className="w-full px-4">
+      <div className="w-full">
         {/* Single Full Banner Image */}
-        <div className="rounded-3xl overflow-hidden shadow-2xl border-4 border-[#f26104]">
+        <div className="overflow-hidden shadow-2xl border-4 ">
           <img
             src={img2}
             alt="AK Crackers Diwali Fireworks Celebration Banner"
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import PromoBanner from "../components/PromoBanner";
 import Header from "../components/Header";
+import AboutUs from "../components/AboutUs";
 import Footer from "../components/Footer";
 function About() {
   return (
     <div>
       <Header />
       <PromoBanner />
+      <AboutUs />
       <Footer />
     </div>
   );
