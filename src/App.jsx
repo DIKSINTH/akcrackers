bbbabaf5a3317ae2d38393f6c02ebbb6990fd113
DIKSINTH <diksinth1@gmail.com>
2026-02-12 14:03:24 +0530

    start-project

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
+import Contact from "./pages/Contact.jsx";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,12 +3,14 @@ import PromoBanner from "../components/PromoBanner";
 import Header from "../components/Header";
 import AboutUs from "../components/AboutUs";
 import Footer from "../components/Footer";
+import VisionMission from "../components/VisionMission";
 function About() {
   return (
     <div>
       <Header />
       <PromoBanner />
       <AboutUs />
+      <VisionMission />
       <Footer />
     </div>
   );
